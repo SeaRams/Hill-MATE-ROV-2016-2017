30dd1a16462264ea59a4194f0d2d46194905d1fa
Divadi <dpark@thehill.org>
2017-01-27 22:18:17 +0000

    working surface socket

--- Surface Pi.py
+++ Surface Pi.py
@@ -3,11 +3,13 @@
 import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#host = "10.0.2.51"
-host = '10.0.2.51'
-port = 9000
+
+host = '169.254.122.176' #eth0 "inet addr" of Sub Pi
+port = 9000 #Arbitrary port above 5000 - must be same for sub Pi
 
 sock.connect((host, port))
+
 print "connected"
-#print(s.recv(1024))
-s.close()
+print(sock.recv(1024))
+#sock.send("thank you for serving")
+sock.close()
