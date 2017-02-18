d8897a128931dcf5821cbe13fcbcf21b8b3f9433
Divadi <dpark@thehill.org>
2017-02-17 22:36:11 -0500

    Major changes, untested

--- src/Sub Pi.py
+++ src/Sub Pi.py
@@ -1,47 +1,80 @@
 #Sub Pi is the server
 import socket
 import pygame
+import time
 from TextPrint import TextPrint
+from Adafruit_PWM_Servo_Driver import PWM
 
 
-def setUpSocket():
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    host = '0.0.0.0'
-    port = 9000
-    sock.bind((host, port))
-    sock.listen(5)
-    print "server is ready"
-    surface, addr = sock.accept()
-    print('Got connection from', addr)
-    surface.send('Thank you for connecting')
+#setup socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = '0.0.0.0'
+port = 9000
+sock.bind((host, port))
+sock.listen(5)
+print "server is ready"
+surface, addr = sock.accept()
+print('Got connection from', addr)
+surface.send('Thank you for connecting')
 
-def setUpScreen():
-    pygame.init()
-    textPrint = TextPrint()
-    size = [500, 500]
-    screen = pygame.display.set_mode(size)
-    pygame.display.set_caption("Joystick Info from: " + str(addr))
-    clock = pygame.time.Clock()
+#setup screen
+pygame.init()
+textPrint = TextPrint()
+size = [500, 500]
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Joystick Info from: " + str(addr))
+clock = pygame.time.Clock()
 
-setUpSocket()
-setUpScreen()
+# Initialise the PWM device using the default address
+pwm = PWM(0x40)
+pwm.setPWMFreq(60)
+
+#IMPORTANT: THESE ARE VALUES FROM LAST YEAR, HAVE NOT BEEN CONFIRMED, DO NOT USE WITHOUT TESTING
+#Servo default pulse lengths. 
+wrist_mid   = 430
+claw_mid    = 335
+arm_mid     = 410
+cam_mid   = 365
+
+#Constant thruster pulse lengths. 
+THRUSTER_MID = 410
+
+#Active values. Only thrusters for now. TO CHANGE WHEN ARM GETS MADE
+#"Left" "Right" when looking at the ROV from behind. (Camera is "front")
+motorLeft = THRUSTER_MID
+motorRight = THRUSTER_MID
+motorBackVertical = THRUSTER_MID
+motorLeftVertical = THRUSTER_MID
+motorRightVertical = THRUSTER_MID
 
 
-joystickInput = surface.recv(24)
+joystickInput = surface.recv(15)
 while joystickInput != "END":
+    motorLeft = joystickInput[0:3]
+    motorRight = joystickInput[3:6]
+    motorBackVertical = joystickInput[6:9]
+    motorLeftVertical = joystickInput[9:12]
+    motorRightVertical = joystickInput[12:15]
+
+    pwm.setPWM(4, 0, motorLeft)
+    pwm.setPWM(5, 0, motorRight)
+    pwm.setPWM(8, 0, motorBackVertical)
+    pwm.setPWM(9, 0, motorLeftVertical)
+    pwm.setPWM(10, 0, motorRightVertical)
+
     textPrint.reset()
-    
-    #print joystickInput
-    joystickInput = surface.recv(24)
-
-    textPrint.printInfo(screen,"Joystick axis:")
+    textPrint.printInfo(screen,"Motor Values:")
     textPrint.indent()
-
-    for i in range(4):
-        textPrint.printInfo(screen,"Axis {} value: {}".format(i, joystickInput[i * 6:i * 6 + 6]))
+    textPrint.printInfo(screen, "motorLeft value: {}".format(motorLeft))
+    textPrint.printInfo(screen, "motorRight value: {}".format(motorRight))
+    textPrint.printInfo(screen, "motorbackVertical value: {}".format(motorBackVertical))
+    textPrint.printInfo(screen, "motorLeftVertical value: {}".format(motorLeftVertical))
+    textPrint.printInfo(screen, "motorRightVertical value: {}".format(motorRightVertical))
     pygame.display.flip()
+
+    time.sleep(0.05)
+    joystickInput = surface.recv(15)
     
 pygame.quit()
 print(c.recv(21)) #ensures that the client "closes" first, or the server port will be stuck on TIME_WAIT
 c.close()
-
--- src/Surface Pi.py
+++ src/Surface Pi.py
@@ -4,36 +4,100 @@ import socket
 import pygame
 from TextPrint import TextPrint
 
-def setUpSocket():
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    host = '169.254.122.176' #eth0 "inet addr" of Sub Pi
-    port = 9000 #Arbitrary port above 5000 - must be same for sub Pi
-    sock.connect((host, port))
-    print "Connected"
-    print(sock.recv(1024))
+# sub = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# host = '169.254.122.176' #eth0 "inet addr" of Sub Pi
+# port = 9000 #Arbitrary port above 5000 - must be same for sub Pi
+# sub.connect((host, port))
+# print "Connected"
+# print(sub.recv(1024))
+
+pygame.init()
+joystick1 = pygame.joystick.Joystick(0) #Was in main while loop, does this work? *NOT TESTED
+joystick1.init()
+print("k")
+running = True
 
 
-done = False
-
-while done == False:
-    joystick1 = pygame.joystick.Joystick(0)
-    joystick1.init()
+#IMPORTANT: THESE ARE VALUES FROM LAST YEAR, HAVE NOT BEEN CONFIRMED, DO NOT USE WITHOUT TESTING
+#Servo default pulse lengths. 
+wrist_minus = 280
+wrist_mid   = 430
+wrist_plus  = 580
     
-    for event in pygame.event.get():
-        if event.type == pygame.JOYBUTTONDOWN:
-            if(joystick1.get_button(11) == 1): #kill command
-                sock.send("END")
-                done = True
-    
-    toSend = ""
-    for i in range(0, 4):
-        axis = str(joystick1.get_axis(i))
-        toSend += str(axis)[0:6]
-        for j in range(0, 6 - len(str(axis))):
-            toSend += "0" #just in case "axis" is 1.0 or too short
-    #print toSend
-    sock.send(toSend)
-    clock.tick(20)
+claw_minus  = 200
+claw_mid    = 335
+claw_plus   = 490
 
-sock.send("thank you for serving")
-sock.close()
+arm_minus   = 270
+arm_mid     = 410
+arm_plus    = 550
+
+cam_mid   = 365
+
+#Constant thruster pulse lengths. 
+THRUSTER_MAX = 600
+THRUSTER_MID = 410
+THRUSTER_MIN = 220
+
+
+#Active values. Only thrusters for now. TO CHANGE WHEN ARM GETS MADE
+#"Left" "Right" when looking at the ROV from behind. (Camera is "front")
+motorLeft = THRUSTER_MID
+motorRight = THRUSTER_MID
+motorFrontVertical = THRUSTER_MID
+motorLeftVertical = THRUSTER_MID
+motorRightVertical = THRUSTER_MID
+
+#Controls how much juice is going to the motors. Ranges from 0% to 100%
+throttle = 0;
+
+def changeInterval(x, in_min, in_max, out_min, out_max):     #x is a value between in_min and in_max. It is "re-mapped" to between out_min and out_max.
+    return int( (x-in_min) * (out_max-out_min) // (in_max-in_min) + out_min )
+
+def processForwardBackward(joystickValue): #remember that moving the joystick forward results in a negative value.
+    newValue = changeInterval(-joystickValue, -1, 1, THRUSTER_MIN, THRUSTER_MAX)
+    motorLeft = newValue
+    motorRight = newValue
+
+def processYaw(joystickValue):
+    return 0;
+
+def processVertical():
+    if(joystick1.get_button(6)): #button labeled "7" is pressed, ROV goes down
+        if(motorFrontVertical > THRUSTER_MIN): #because there are two back motors and one front, the back ones go half as fast as the front ones so the ROV doesn't flip.
+            motorFrontVertical = motorFrontVertical - 2
+            motorLeftVertical = motorLeftVertical - 1
+            motorRightVertical = motorRightVertical - 1
+
+    elif(joystick1.get_button(7)): #button labeled "8" is pressed, ROV goes up
+        if(motorFrontVertical < THRUSTER_MAX): #because there are two back motors and one front, the back ones go half as fast as the front ones so the ROV doesn't flip.
+            motorFrontVertical = motorFrontVertical + 2
+            motorLeftVertical = motorLeftVertical + 1
+            motorRightVertical = motorRightVertical + 1
+
+    else: #neither button was pressed, ROV will stop moving vertically
+        motorFrontVertical = THRUSTER_MID
+        motorLeftVertical = THRUSTER_MID
+        motorRightVertical = THRUSTER_MID
+
+def processJoystick():
+    if(joystick1.get_button(0) == 1): #trigger button ends program
+        running = False
+    processForwardBackward(joystick1.get_axis(1)) #"1" should be the forward-backwards axis. CHECK THIS
+    processYaw(joystick1.get_axis(2)) #"2" should be the twist axis. CHECK THIS
+    processVertical()
+
+def packageInformation():
+    toSend = str(motorLeft) + str(motorRight) + str(motorFrontVertical) + str(motorLeftVertical) + str(motorRightVertical)
+    return toSend
+
+while running:
+    processJoystick()
+    if(running):
+        sub.send(packageInformation())
+    else:
+        sub.send("END")
+
+    time.sleep(0.05) #every 50 milliseconds
+
+sub.close()
